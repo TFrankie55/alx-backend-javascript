53b02279d3726ff40445563c0023ead254f76174
TFrankie55 <towoeye50@gmail.com>
2023-01-24 16:43:41 -0800

    description

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,1 +1,15 @@
-ynjs
+export function taskFirst() {
+	  var task = 'I prefer const when I can.';
+	  return task;
+}
+
+export function getLast() {
+	  return ' is okay';
+}
+
+export function taskNext() {
+	  var combination = 'But sometimes let';
+	  combination += getLast();
+
+	  return combination;
+}
