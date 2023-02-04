29d6125a5ee1ba29ba41c918863094b44ef537a6
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-02-04 06:51:50 +0100

    Update java.ts

--- 0x04-TypeScript/task_4/js/subjects/java.ts
+++ 0x04-TypeScript/task_4/js/subjects/java.ts
@@ -1,1 +1,18 @@
-java.ts
+/// <reference path="Teacher.ts" />
+/// <reference path="Subjects.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class JavaClass extends Subject{
+    getRequirements = () => 'Here is the list of requirements for Java';
+
+    getAvailableTeacher = () => {
+      if (!this.teacher?.experienceTeachingJava) return 'No available teacher';
+
+      return `Available Teacher: ${this.teacher}`;
+    } 
+  }
+}
