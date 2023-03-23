9a0a353662ecc13f4623f7dddcb8c98a68eb4445
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2023-03-23 13:15:07 +0100

    corrected http module

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,25 +1,35 @@
-const express = require('express');
+const http = require('http');
 
 const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const app = express();
+const DATABASE = args[0];
+
+const hostname = '127.0.0.1';
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
 
-app.get('/students', async (req, res) => {
-  const heading = 'This is the list of our students\n';
-  try {
-    const students = await countStudents(args[0]);
-    res.send(`${heading}${students.join('\n')}`);
-  } catch (error) {
-    res.send(`${heading}${error.message}`);
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
   }
+  res.statusCode = 404;
+  res.end();
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+});
 
 module.exports = app;
